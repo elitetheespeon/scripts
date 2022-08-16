f996f6bb0adb9ab2e2dbe3c34b51bb5a1e126a80
elitetheespeon <l337espeon@gmail.com>
2022-08-16 05:50:41 -0400

    Update wireguard_iptables.sh

--- wireguard_iptables.sh
+++ wireguard_iptables.sh
@@ -1,10 +1,12 @@
-## Set outbound IP to second IP
-/sbin/iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to 2.2.2.2
-## Allow outbound traffic forwarding for Wireguard network
+## Set outbound IP to second IP (2.2.2.2) when traffic from wg subnet (10.109.144.0/24) goes outbound to internet (eth0)
+/sbin/iptables -t nat -A POSTROUTING -s 10.109.144.0/24 -o eth0 -j SNAT --to-source 2.2.2.2
+
+## Allow traffic forwarding for Wireguard network (wg0) when traffic goes outbound to internet (eth0)
 /sbin/iptables -A FORWARD -i eth0 -o wg0 -j ACCEPT
 /sbin/iptables -A FORWARD -i wg0 -j ACCEPT
 /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-## Forward traffic for 2.2.2.2 SSH to Wireguard client 10.109.144.2
+
+## Forward traffic for 2.2.2.2 TCP port 22 to Wireguard client 10.109.144.2
 /sbin/iptables -t nat -A PREROUTING -d 2.2.2.2 -p tcp --dport 22 -j DNAT --to-destination 10.109.144.2
 
 ## Where <2.2.2.2> would be public VPN IP and <10.109.144.2> would be private Wireguard client IP to direct traffic to.
